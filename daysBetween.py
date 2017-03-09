2ed82740bc66cabe85203ce53971b0660ca2fb99
RDP <rdupaw@gmail.com>
2017-03-09 13:30:52 -0600

    Add if/else to daysInMonth to add correct amount of days to each month

--- daysBetween.py
+++ daysBetween.py
@@ -1,6 +1,12 @@
 #counts days inbetween birth ro current day taking into account leap years.
 def daysInMonth(year, month):
-    return 30    
+    if month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
+        return 31
+    else:
+        if month == 2:
+            return 28
+        else:
+            return 30    
 
 def nextDay(year, month, day):
     """
