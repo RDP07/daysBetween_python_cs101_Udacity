4ec41620049a4eaa3eb37afd8c646ae90c99875f
RDP <rdupaw@gmail.com>
2017-03-09 13:51:28 -0600

    Edit if in daysInMonth to be more cleanly written

--- daysBetween.py
+++ daysBetween.py
@@ -12,7 +12,7 @@ def isLeapYear(year):
 
 def daysInMonth(year, month):
     """Correct version with correct days in each month and accounting for leap years."""
-    if month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
+    if month in (1, 3, 5, 7, 8, 10, 12):
         return 31
     else:
         if month == 2:
